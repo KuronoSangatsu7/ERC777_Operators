e5dcb687c86aa74baa99213d12d6d9c3c4fcd0eb
Jaffar Totanji <jacepac264@gmail.com>
2022-06-18 21:50:24 +0300

    added unit test for BulkSender and FixedPriceSeller operators

---
+++ test/BulkSender.test.js
@@ -0,0 +1,86 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { singletons } = require("@openzeppelin/test-helpers");
+
+describe("BulkSender", function () {
+
+    let token, bulkSender, accounts;
+
+    beforeEach(async () => {
+        accounts = await ethers.getSigners();
+        accounts = accounts.map(x => x.address);
+
+        const BulkSender = await ethers.getContractFactory("BulkSender");
+        bulkSender = await BulkSender.deploy();
+        await bulkSender.deployed();
+
+        let initialSupply = 1000;
+        let defaultOperators = [bulkSender.address];
+
+        this.erc1820 = await singletons.ERC1820Registry("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
+
+        const CheapAToken = await hre.ethers.getContractFactory("CheapAToken");
+        token = await CheapAToken.deploy(initialSupply, defaultOperators);
+        await token.deployed();
+    });
+
+    it("initializes successfully", async function() {
+        expect(bulkSender.address).to.be.properAddress;
+        expect(token.address).to.be.properAddress;
+        const [bulkSendOperator] = await token.defaultOperators();
+        expect(bulkSendOperator).to.equal(bulkSender.address);
+    });
+
+    it("bulk transfers the same amount", async function () {
+
+        let recipientAddresses = accounts;
+        recipientAddresses.shift();
+
+        let recipientBalancesBefore = await Promise.all(recipientAddresses.map(async (i) => {
+            let balance = new ethers.BigNumber.from(await token.balanceOf(i));
+            balance = balance.toNumber();
+            return balance;
+        }));
+
+        await bulkSender.send(token.address, recipientAddresses, '2', [])
+
+        let recipientBalancesAfter = await Promise.all(recipientAddresses.map(async (i) => {
+            let balance = new ethers.BigNumber.from(await token.balanceOf(i));
+            balance = balance.toNumber();
+            return balance;
+        }));
+
+        recipientBalancesBefore.forEach((n, index) => {
+            const m = recipientBalancesAfter[index];
+            expect(m).to.equal(n + 2);
+        });
+    });
+
+    it("bulk transfers different amounts", async function () {
+
+        let recipientAddresses = accounts.slice(1, 6);
+
+        let recipientBalancesBefore = await Promise.all(recipientAddresses.map(async (i) => {
+            let balance = new ethers.BigNumber.from(await token.balanceOf(i));
+            balance = balance.toNumber();
+            return balance;
+        }));
+
+        let amountsToSend = ['1', '2', '3', '4', '5'];
+
+        await bulkSender.sendAmounts(token.address, recipientAddresses, amountsToSend, [])
+
+        let recipientBalancesAfter = await Promise.all(recipientAddresses.map(async (i) => {
+            let balance = new ethers.BigNumber.from(await token.balanceOf(i));
+            balance = balance.toNumber();
+            return balance;
+        }));
+
+        recipientBalancesBefore.forEach((n, index) => {
+            const m = recipientBalancesAfter[index];
+            const amount = parseInt(amountsToSend[index]);
+            expect(m).to.equal(n + amount);
+        });
+    });
+  });
+  
---
+++ test/FixedPriceSeller.test.js
@@ -0,0 +1,98 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { singletons } = require("@openzeppelin/test-helpers");
+
+describe("FixedPriceSeller", function () {
+
+    let token, fixedPriceSeller, accounts, signers;
+
+    beforeEach(async () => {
+        signers = await ethers.getSigners();
+
+        accounts = signers.map(x => x.address);
+
+        const FixedPriceSeller = await ethers.getContractFactory("FixedPriceSeller");
+        fixedPriceSeller = await FixedPriceSeller.deploy();
+        await fixedPriceSeller.deployed();
+
+        let initialSupply = 1000;
+        let defaultOperators = [fixedPriceSeller.address];
+
+        this.erc1820 = await singletons.ERC1820Registry(accounts[0]);
+
+        const CheapAToken = await hre.ethers.getContractFactory("CheapAToken");
+        token = await CheapAToken.deploy(initialSupply, defaultOperators);
+        await token.deployed();
+    });
+
+    it("initializes successfully", async function() {
+        expect(fixedPriceSeller.address).to.be.properAddress;
+        expect(token.address).to.be.properAddress;
+        const [fixedPriceOperator] = await token.defaultOperators();
+        expect(fixedPriceOperator).to.equal(fixedPriceSeller.address);
+    });
+
+    it("sets price for a token", async function() {
+        await fixedPriceSeller.setPricePerToken(token.address, ethers.utils.parseUnits("1.0"));
+        let price = await fixedPriceSeller.getPricePerToken(token.address, accounts[0]);
+        price = ethers.utils.formatUnits(price.toString())
+        expect(price).to.equal('1.0');
+    });
+
+    it("sells tokens", async function() {
+        //buying 10 tokens for 10 ether after setting the price per token to 1 ether
+        await fixedPriceSeller.setPricePerToken(token.address, ethers.utils.parseUnits("1.0"));
+        const value = ethers.utils.parseUnits("10");
+
+        await fixedPriceSeller.connect(signers[1]).send(token.address, accounts[0], {value: value})
+
+        let holderCATBalance = await token.balanceOf(accounts[0]);
+        let buyerCATBalance = await token.balanceOf(accounts[1]);
+
+        expect(holderCATBalance).to.equal('990')
+        expect(buyerCATBalance).to.equal('10')
+    });
+
+    it("does not sell when a price is not set", async function() {
+        const value = ethers.utils.parseUnits("10");
+
+        await expect(fixedPriceSeller.connect(signers[1]).send(token.address, accounts[0], {value: value})).to.be.reverted;
+    });
+
+    it("does not sell when the price is set to 0", async function() {
+        await fixedPriceSeller.setPricePerToken(token.address, ethers.utils.parseUnits("0"))
+        const value = ethers.utils.parseUnits("10");
+
+        await expect(fixedPriceSeller.connect(signers[1]).send(token.address, accounts[0], {value: value})).to.be.reverted;
+    });
+
+    it("does not sell when not paid", async function() {
+        await fixedPriceSeller.setPricePerToken(token.address, ethers.utils.parseUnits("1.0"))
+        const value = ethers.utils.parseUnits("10");
+
+        await expect(fixedPriceSeller.connect(signers[1]).send(token.address, accounts[0])).to.be.reverted;
+    });
+
+    it("does not sell non-integer number of tokens", async function() {
+        //buying tokens for 11 ether after setting the price per token to 2.5 ether
+        await fixedPriceSeller.setPricePerToken(token.address, ethers.utils.parseUnits("2.5"))
+        const value = ethers.utils.parseUnits("11");
+
+        await expect(fixedPriceSeller.connect(signers[1]).send(token.address, accounts[0], {value: value})).to.be.reverted;
+    });
+
+    it("can be revoked", async function() {
+        await token.revokeOperator(fixedPriceSeller.address);
+        let isOperator = await token.isOperatorFor(fixedPriceSeller.address, accounts[0]);
+        await expect(isOperator).to.be.false;
+    });
+
+    it("does not sell after being revoked", async function() {
+        await fixedPriceSeller.setPricePerToken(token.address, ethers.utils.parseUnits("1"))
+        const value = ethers.utils.parseUnits("10");
+        await token.revokeOperator(fixedPriceSeller.address);
+
+        await expect(fixedPriceSeller.connect(signers[1]).send(token.address, accounts[0], {value: value})).to.be.reverted;
+    });
+
+});
