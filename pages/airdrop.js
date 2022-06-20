928c1f50b1fe32fed0cf2299f2f8dd09ff110271
Jaffar <jacepac264@gmail.com>
2022-06-21 00:26:47 +0300

    configured same amount and varying amounts bulk-send

--- pages/airdrop.js
+++ pages/airdrop.js
@@ -4,12 +4,15 @@ export default function AirDrop() {
   const [numOfAddresses, setNumOfAddresses] = useState(0);
   const [addresses, setAddresses] = useState([]);
   const [amounts, setAmounts] = useState([]);
+  const [sameAmountVal, setSingleAmountval] = useState(0);
+  const [sameAmount, setSameAmount] = useState(false);
 
   useEffect(() => {
     const generateArray = (num) => {
       return Array.from(new Array(num).map((_) => " "));
     };
     numOfAddresses && setAddresses(generateArray(numOfAddresses));
+    numOfAddresses && setAmounts(generateArray(numOfAddresses));
   }, [numOfAddresses]);
 
   const handleUserAddressChange = (position) => (e) => {
@@ -20,16 +23,16 @@ export default function AirDrop() {
     ]);
   };
 
-  /*const handleUserAmountChange = (position) => (e) => {
-    setAddresses(
-      ...addresses.slice(0, position),
+  const handleUserAmountChange = (position) => (e) => {
+    setAmounts([
+      ...amounts.slice(0, position),
       e.target.value,
-      ...addresses.slice(position + 1)
-    );
+      ...amounts.slice(position + 1),
+    ]);
   };
-  */
-  const showMeAddresses = () => {
-    console.log(addresses);
+
+  const showMe = () => {
+    console.log(addresses, sameAmount ? sameAmountVal : amounts);
   };
   return (
     <div className="columns is-centered">
@@ -43,7 +46,7 @@ export default function AirDrop() {
                   className="input"
                   type="number"
                   min="0"
-                  max="100"
+                  max="50"
                   value={numOfAddresses}
                   onChange={(e) => setNumOfAddresses(parseInt(e.target.value))}
                 ></input>
@@ -53,35 +56,53 @@ export default function AirDrop() {
             <div className="field">
               <div className="control">
                 <label className="checkbox">
-                  <input type="checkbox"></input> Send the same amount to all
-                  addresses
+                  <input
+                    type="checkbox"
+                    onChange={() => setSameAmount(!sameAmount)}
+                  ></input>{" "}
+                  Send the same amount to all addresses
                 </label>
               </div>
             </div>
-          </fieldset>
-
-          <fieldset>
-            <div className="field">
-              <div className="control">
-                {addresses.map((address, i) => (
-                  <div key={i}>
+            {addresses.map((address, i) => (
+              <div className="field" key={i}>
+                <div>
+                  <div className="is-flex-direction-row">
                     <label className="label">Address {i + 1}: </label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        value={address || ""}
+                        onChange={handleUserAddressChange(i)}
+                      ></input>
+                    </div>
+                  </div>
+                  <label className="label">Amount {i + 1}: </label>
+                  <div className="control">
                     <input
                       className="input"
                       type="text"
-                      value={address || ""}
-                      onChange={handleUserAddressChange(i)}
+                      value={
+                        sameAmount
+                          ? sameAmountVal
+                            ? sameAmountVal
+                            : ""
+                          : amounts[i] || ""
+                      }
+                      onChange={sameAmount? e => setSingleAmountval(parseInt(e.target.value)): handleUserAmountChange(i)}
                     ></input>
                   </div>
-                ))}
+                </div>
+              </div>
+            ))}
+            <div className="field is-grouped-centered">
+              <div className="control">
+                <button className="button is-danger" onClick={() => showMe()}>
+                  Send Tokens
+                </button>
               </div>
             </div>
-            <button
-              className="button is-danger"
-              onClick={() => showMeAddresses()}
-            >
-              Send Tokens
-            </button>
           </fieldset>
         </div>
       </div>
