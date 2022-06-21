fabbb3c44ebe48478738ee09a68eaa9cb46a81b2
Jaffar <jacepac264@gmail.com>
2022-06-21 15:31:26 +0300

    added Listing modal to static price seller UI

--- pages/selltokens.js
+++ pages/selltokens.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 
 export default function SellTokens() {
-  const [numOfListings, setNumOfListings] = useState(3);
+  const [numOfListings, setNumOfListings] = useState(5);
   const [listings, setListings] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [listedPrice, setListedPrice] = useState(0);
 
   useEffect(() => {
     const generateArray = (num) => {
@@ -12,7 +14,7 @@ export default function SellTokens() {
   }, [numOfListings]);
 
   const showMe = () => {
-    console.log(numOfListings, listings);
+    console.log(numOfListings, listings, listedPrice);
   };
 
   return (
@@ -43,7 +45,10 @@ export default function SellTokens() {
                     </div>
 
                     <div className="field column is-offset-6">
-                      <button className="control button is-primary is-medium is-fullwidth" onClick={showMe}>
+                      <button
+                        className="control button is-primary is-medium is-fullwidth"
+                        onClick={showMe}
+                      >
                         Buy
                       </button>
                     </div>
@@ -51,8 +56,46 @@ export default function SellTokens() {
                 </div>
               ))}
             </fieldset>
-            <button className="control button is-link is-large is-fullwidth mt-5"> List your CAT's </button>
+            <button
+              className="control button is-dark is-large is-fullwidth mt-5"
+              onClick={() => setShowModal(true)}
+            >
+              +
+            </button>
           </div>
+        </div>
+      </div>
+
+      <div className={"modal " + (showModal ? "is-active" : "")}>
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">List CATs</p>
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={() => setShowModal(false)}
+            ></button>
+          </header>
+          <section className="modal-card-body">
+            <div>
+              Put your <strong className="is-italic">CheapATokens</strong> up
+              for sale for a static price.
+            </div>
+            <label className="label mt-4 is-size-5">
+              Price:
+            </label>
+            <div className="columns mt-2 ml-2">
+            <input className="input column is-5 mt-4" value={listedPrice} onChange={(e) => setListedPrice(e.target.value)}></input>
+            <div className="label column is-size-3"> ETH </div>
+            </div>
+          </section>
+          <footer className="modal-card-foot">
+            <button className="button is-dark" onClick={() => {showMe(); setShowModal(false)}}>List Tokens</button>
+            <button className="button" onClick={() => setShowModal(false)}>
+              Cancel
+            </button>
+          </footer>
         </div>
       </div>
     </>
