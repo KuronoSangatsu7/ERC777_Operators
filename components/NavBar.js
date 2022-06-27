fe6fda918599cb7855b0852cfbbb7c52212f52fc
Jaffar <jacepac264@gmail.com>
2022-06-27 22:43:31 +0300

    added current connected account to navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,18 +1,18 @@
 import Link from "next/link";
+import { useWallet } from "./wallet-context";
 
 export default function NavBar() {
+  const { connectedAccount, currentChain, loaded } = useWallet();
   return (
     <>
       <nav className="navbar is-fixed-top is-white has-shadow">
-        
         <div className="navbar-brand">
           <a href="/">
-          <img className="navbar-item" src="mustaš.svg"></img>
+            <img className="navbar-item" src="mustaš.svg"></img>
           </a>
         </div>
 
         <div className="navbar-menu">
-
           <div className="navbar-start">
             <a className="navbar-item" href="airdrop">
               Air Drop UI
@@ -21,9 +21,23 @@ export default function NavBar() {
               Static Price Seller
             </a>
           </div>
-          
+          {connectedAccount ? (
+            <>
+              <div className="navbar-end">
+                <div className="navbar-item columns is-gapless">
+                  <div className="label column mr-2 has-text-primary">
+                    Connected Account:
+                  </div>
+                  <div className="is-family-monospace column has-text-weight-bold">
+                    {connectedAccount}
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
-
       </nav>
     </>
   );
